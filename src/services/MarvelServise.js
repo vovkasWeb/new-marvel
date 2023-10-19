1838ab65cd44e76fe8733178347912022205543b
vovkasWeb <vova77557@gmail.com>
2023-10-19 19:36:51 +0300

    fix: api key

--- src/services/MarvelServise.js
+++ src/services/MarvelServise.js
@@ -1,6 +1,6 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/'
-	_apiKey = 'apikey=f2480d5ed6615bb113e67eb7eebaad38'
+	_apiKey = 'apikey=17c90dff8350bf52ea40e82d4e7f7550'
 	getResource = async url => {
 		let res = await fetch(url)
 
